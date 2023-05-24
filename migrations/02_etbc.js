7d8e5a0f4a4e4a6e00b614d1821f50fb810a59ac
Eyasuk <eyasukibru290@gmail.com>
2023-05-24 11:40:39 +0300

    Etbc coin created | demo eth added

--- migrations/02_etbc.js
+++ migrations/02_etbc.js
@@ -1,5 +1,5 @@
 const Etbc = artifacts.require("Etbc");
 
 module.exports = function(deployer) {
-    deployer.deploy(Etbc,5777);
+    deployer.deploy(Etbc,"0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",250000000);
 };
